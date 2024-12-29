67d65eb56f132019115f501c333443892f5593ec
oprev.a <oprev.a@paspay.kz>
2024-12-29 07:45:21 +0500

    TC #5 update SubmitOrderPage

--- src/main/java/com/diploma/practice/pages/SubmitOrderPage.java
+++ src/main/java/com/diploma/practice/pages/SubmitOrderPage.java
@@ -9,10 +9,15 @@ import java.time.Duration;
 
 public class SubmitOrderPage {
     public SelenideElement checkoutDescriptionArea = Selenide.$("#checkout_description");
+    public SelenideElement confirmOrderButton = Selenide.$("button.btn.btn-primary[type='submit']");
+    SelenideElement personalDataFieldset = Selenide.$("div.col-md-6 fieldset legend");
 
     @Step("Waiting for submit order page opened")
     public void waitSubmitOrderPageLoaded(){
         checkoutDescriptionArea.shouldBe(Condition.visible, Duration.ofSeconds(8));
+        personalDataFieldset.shouldBe(Condition.visible, Duration.ofSeconds(8));
+        confirmOrderButton.scrollTo();
+        confirmOrderButton.shouldBe(Condition.visible, Duration.ofSeconds(8));
     }
 
 }
